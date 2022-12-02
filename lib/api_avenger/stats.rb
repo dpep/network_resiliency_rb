a0473fbef911763b3bd03a1cd4ccf4547f3abec6
Daniel Pepper <pepper.daniel@gmail.com>
2022-12-02 10:03:08 -0800

    upgrade / fix stats

--- lib/api_avenger/stats.rb
+++ lib/api_avenger/stats.rb
@@ -1,10 +1,21 @@
 module ApiAvenger
   class Stats
-    attr_reader :n, :avg, :variance
+    attr_reader :n, :avg
+
+    def self.from(n, avg, sq_dist)
+      new.tap do |instance|
+        instance.instance_eval do
+          @n = n
+          @avg = avg
+          @sq_dist = sq_dist
+        end
+      end
+    end
 
     def initialize(values = [])
       @n = 0
-      @avg = @variance = 0.0
+      @avg = 0.0
+      @sq_dist = 0.0 # sum of squared distance from mean
 
       values.each {|x| update(x) }
     end
@@ -22,6 +33,10 @@ module ApiAvenger
       self
     end
 
+    def variance(sample: false)
+      @sq_dist / (sample ? (@n + 1) : @n)
+    end
+
     def stdev
       Math.sqrt(variance)
     end
@@ -34,29 +49,42 @@ module ApiAvenger
     def merge!(other)
       raise ArgumentError unless other.is_a?(self.class)
 
+      prev_n = n
       @n += other.n
-      @avg += (other.avg - avg) * other.n / n
 
-      # approximate
-      @variance += (other.variance - variance) * other.n / n
+      delta = other.avg - avg
+      @avg += delta * other.n / n
+
+      @sq_dist += other.instance_variable_get(:@sq_dist)
+      @sq_dist += (delta ** 2) * prev_n * other.n / n
 
       self
     end
 
     private
 
-    def update(value, weight: 1)
-      raise ArgumentError if weight < 0
+    def update(value)
+      raise ArgumentError unless value.is_a?(Numeric)
 
-      return if value.nil? || weight == 0
-
-      @n += weight
+      @n += 1
 
       prev_avg = @avg
-      @avg += (value - @avg) * weight / @n
+      @avg += (value - @avg) / @n
 
-      sq_diff = (value - prev_avg) * (value - @avg)
-      @variance += (sq_diff - @variance) * weight / @n
+      @sq_dist += (value - prev_avg) * (value - @avg)
+      # @sq_dist += (sq_dist - @sq_dist) / @n
+
+# for x, w in data_weight_pairs:
+#   w_sum = w_sum + w
+#   mean_old = mean
+#   mean = mean_old + (w / w_sum) * (x - mean_old)
+#   S = S + w * (x - mean_old) * (x - mean)
+
+  # count += 1
+  # delta = newValue - mean
+  # mean += delta / count
+  # delta2 = newValue - mean
+  # M2 += delta * delta2
     end
   end
 end
--- spec/stats_spec.rb
+++ spec/stats_spec.rb
@@ -1,94 +1,243 @@
 describe ApiAvenger::Stats do
-  let(:precision) { 0.001 } # percent
-  let(:stats) { described_class.new }
+  subject(:stats) { described_class.new }
 
-  def calc_avg(values)
-    values.sum(0.0) / values.count
+  let(:precision) { 0.00001 }
+  let(:data) { [] }
+
+  def calc_avg
+    data.flatten!
+    data.sum(0.0) / data.count
   end
 
-  def calc_stdev(values)
-    avg = calc_avg(values)
-    sq_diff = values.sum(0.0) {|x| (x - avg) ** 2 }
-    Math.sqrt(sq_diff / values.count)
+  def calc_stdev
+    avg = calc_avg
+    sq_diff = data.sum(0.0) { |x| (x - avg) ** 2 }
+    Math.sqrt(sq_diff / data.count)
+  end
+
+  def choose(limit: 100_000)
+    # rand(-100_000..100_000)
+    rand * limit * 2 - limit
   end
 
   describe 'helper methods' do
     specify 'sanity check' do
-      data = [ 1, 2, 3, 4, 5 ]
+      data << [ 1, 2, 3, 4, 5 ]
 
-      expect(calc_avg(data)).to eq 3
-      expect(calc_stdev(data)).to eq(Math.sqrt(2))
+      expect(calc_avg).to eq 3
+      expect(calc_stdev).to eq(Math.sqrt(2))
     end
 
     specify 'another sanity check' do
-      data = 1..31
+      data << (1..31).to_a
 
-      expect(calc_avg(data)).to eq 16
-      expect(calc_stdev(data)).to be_within(precision).percent_of(8.94427)
+      expect(calc_avg).to eq 16
+      expect(calc_stdev).to be_within(precision).of(8.94427)
     end
 
     specify 'yet another' do
-      data = [ 10, 12, 23, 23, 16, 23, 21, 16 ]
+      data << [ 10, 12, 23, 23, 16, 23, 21, 16 ]
 
-      expect(calc_avg(data)).to eq 18
-      expect(calc_stdev(data)).to be_within(precision).percent_of(4.898979)
+      expect(calc_avg).to eq 18
+      expect(calc_stdev).to be_within(precision).of(4.898979)
+    end
+  end
+
+  describe '#n' do
+    it 'starts at 0' do
+      expect(stats.n).to be 0
+    end
+
+    it 'accumulates correctly' do
+      1_000.times do |i|
+        stats << 0
+
+        expect(stats.n).to be (i + 1)
+      end
+
+      expect(stats.n).to be 1_000
+    end
+  end
+
+  describe '#avg' do
+    it do
+      1_000.times do |i|
+        stats << 3
+
+        expect(stats.avg).to eq 3
+      end
+    end
+
+    def check
+      expect(stats.avg).to be_within(precision).of(calc_avg)
+    end
+
+    it do
+      1_000.times do |i|
+        data << i
+        stats << i
+
+        check
+      end
+    end
+
+    it 'works with random numbers' do
+      1_000.times.map { choose }.each do |x|
+        data << x
+        stats << x
+
+        check
+      end
+
+      expect(stats.n).to eq 1_000
+    end
+
+    it 'works with LOTS of random numbers' do
+      100_000.times.map { choose }.each_slice(10_000) do |nums|
+        data << nums
+        stats << nums
+
+        check
+      end
+
+      expect(stats.n).to eq 100_000
+    end
+  end
+
+  describe '#stdev' do
+    it do
+      1_000.times do |i|
+        stats << 3
+
+        expect(stats.stdev).to eq 0
+      end
+    end
+
+    def check
+      expect(stats.stdev).to be_within(precision).of(calc_stdev)
+    end
+
+    it do
+      1_000.times do |i|
+        data << i
+        stats << i
+
+        check
+      end
+    end
+
+    it 'works with random numbers' do
+      1_000.times.map { choose }.each do |x|
+        data << x
+        stats << x
+
+        check
+      end
+
+      expect(stats.n).to eq 1_000
+    end
+
+    it 'works with LOTS of random numbers' do
+      100_000.times.map { choose }.each_slice(10_000) do |nums|
+        data << nums
+        stats << nums
+
+        check
+      end
+
+      expect(stats.n).to eq 100_000
     end
   end
 
   describe '#<<' do
+    def check
+      data.flatten!
+      expect(stats.n).to eq data.count
+      expect(stats.avg).to be_within(precision).of(calc_avg)
+      expect(stats.stdev).to be_within(precision).of(calc_stdev)
+    end
+
     it 'calculates the running average and stdev' do
-      data = []
+      1_000.times.map { choose }.each do |x|
+        data << x
+        stats << x
 
-      1000.times do
-        val = rand * 100
+        check
+      end
+    end
 
-        data << val
-        stats << val
+    it 'works with floats also' do
+      1_000.times.map { rand * 200 - 100 }.each do |x|
+        data << x
+        stats << x
 
-        expect(stats.n).to eq data.count
-        expect(stats.avg).to be_within(precision).percent_of(calc_avg(data))
-        expect(stats.stdev).to be_within(precision).percent_of(calc_stdev(data))
+        check
       end
     end
 
     it 'accepts an array of values' do
-      data = []
+      values = (1..100).to_a
 
-      10.times do
-        values = rand(100).times.map { rand * 100 }
+      data << values
+      stats << values
 
-        data += values
-        stats << values
-
-        expect(stats.n).to eq data.count
-        expect(stats.avg).to be_within(precision).percent_of(calc_avg(data))
-        expect(stats.stdev).to be_within(precision).percent_of(calc_stdev(data))
-      end
+      check
     end
 
-    it 'accepts another ApiAvenger::Stats object' do
-      data = []
+    it 'accepts another Stats object' do
+      stats << 1
+      stats << 2
 
-      100.times do
-        values = rand(100).times.map { rand * 100 }
+      more_stats = described_class.new
+      more_stats << [ 3, 4, 5 ]
+      stats << more_stats
 
-        data += values
+      data << [ 1, 2, 3, 4, 5 ]
+
+      check
+    end
+
+    it 'accepts many Stats objects' do
+      other_stats = described_class.new
+
+      expect(stats).to receive(:merge!).with(other_stats)
+      stats << other_stats
+    end
+  end
+
+  describe '#merge' do
+    it do
+      1_000.times do |i|
+        values = rand(10..1_000).times.map { choose }
+
+        data << values
         stats << described_class.new(values)
 
-        expect(stats.n).to eq data.count
-        expect(stats.avg).to be_within(precision).percent_of(calc_avg(data))
-
-        expect(stats.stdev).to be_within(2).percent_of(calc_stdev(data))
-        expect(stats.stdev).to be_within(precision).percent_of(calc_stdev(data))
+        if i % 100 == 0
+          data.flatten!
+          expect(stats.n).to eq data.count
+          expect(stats.avg).to be_within(precision).of(calc_avg)
+          expect(stats.stdev).to be_within(precision).of(calc_stdev)
+        end
       end
+
+      expect(stats.n).to be >= 10_000
     end
 
-    it 'works with small fractions' do
-      data = 1000.times.map { rand }
-      data.each {|x| stats << x }
+    # too slow to run regularly, but works with 100M data points
+    # it 'works with LOTS of random numbers' do
+    #   1_000_000.times do |i|
+    #     values = rand(10..1_000).times.map { choose }
 
-      expect(stats.avg).to be_within(precision).percent_of(calc_avg(data))
-      expect(stats.stdev).to be_within(precision).percent_of(calc_stdev(data))
-    end
+    #     data << values
+    #     stats << described_class.new(values)
+
+    #     if i % 10_000 == 0
+    #       puts "[#{i}] #{stats.n}"
+
+    #       expect(stats.stdev).to be_within(precision).of(calc_stdev)
+    #     end
+    #   end
+    # end
   end
 end
