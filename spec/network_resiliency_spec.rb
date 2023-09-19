e8e06ea253b52c1427d6250c0d272b74bc8b59ad
Daniel Pepper <pepper.daniel@gmail.com>
2023-09-18 20:15:26 -0700

    cleanup

--- spec/network_resiliency_spec.rb
+++ spec/network_resiliency_spec.rb
@@ -1,20 +1,44 @@
 describe NetworkResiliency do
-  describe ".time" do
+  describe ".timestamp" do
+    before do
+      allow(Process).to receive(:clock_gettime).and_return(1, 2)
+    end
+
     it "converts to milliseconds" do
-      ts = NetworkResiliency.time { sleep 0.001 }
+      ts = -NetworkResiliency.timestamp
+      ts += NetworkResiliency.timestamp
 
-      expect(ts).to be_within(1).of(1)
+      expect(ts).to be 1_000
+    end
+  end
+
+  describe ".enabled?" do
+    subject { NetworkResiliency.enabled? }
+
+    it "defaults to true" do
+      is_expected.to be true
     end
 
-    it do
-      ts = NetworkResiliency.time { sleep 0.01 }
+    context "when set to false" do
+      before { NetworkResiliency.enabled = false }
 
-      expect(ts).to be_within(1).of(10)
+      it { is_expected.to be false }
     end
 
-    it "has a min of 1 ms" do
-      ts = NetworkResiliency.time {}
-      expect(ts).to be_within(1).of(0)
+    context "when set to a method" do
+      before do
+        NetworkResiliency.enabled = proc { true }
+      end
+
+      it { is_expected.to be true }
+    end
+
+    context "when given an invalid argument" do
+      it "fails fast" do
+        expect {
+          NetworkResiliency.enabled = 1
+        }.to raise_error(ArgumentError)
+      end
     end
   end
 end
