7febfe0b59b76b72ae5e51972dc74a75cdcd78a3
Daniel Pepper <pepper.daniel@gmail.com>
2023-10-01 13:41:41 -0700

    .

--- spec/http_spec.rb
+++ spec/http_spec.rb
@@ -18,7 +18,7 @@ describe NetworkResiliency::Adapter::HTTP, :mock_socket do
     end
 
     it "patches globally" do
-      stub_const("Net::HTTP", Class.new)
+      stub_const("Net::HTTP", Class.new(Net::HTTP))
 
       described_class.patch
 
